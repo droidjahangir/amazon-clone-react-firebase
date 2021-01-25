42e6594581b7297929a5efe1df196c59e1dba963
jahangir_devs <jahangir.droid@gmail.com>
2021-01-25 17:29:24 +0600

    project initialized

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXhVc0MWmJOgYDetYGKNFcADBKzGMm1-Q",
+  authDomain: "challenge-617a5.firebaseapp.com",
+  projectId: "challenge-617a5",
+  storageBucket: "challenge-617a5.appspot.com",
+  messagingSenderId: "759630220377",
+  appId: "1:759630220377:web:7b9bf866617691b4a47548"
+};
